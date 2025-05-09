5f76b45d6b75cb4c4a3cdfd332108b908330755b
marcbeil <marcbeil2001@gmail.com>
2025-05-09 13:37:12 +0200

    fix bugs

--- server/src/main/java/de/tum/aet/devops25/w03/controller/CanteenController.java
+++ server/src/main/java/de/tum/aet/devops25/w03/controller/CanteenController.java
@@ -20,12 +20,18 @@ public class CanteenController {
 
     /**
      * Get today's meals for a specific canteen
+     *
      * @param canteenName the ID of the canteen (e.g., "mensa-garching")
      * @return list of dishes available today at the specified canteen
      */
     @GetMapping("/{canteenName}/today")
-        public ResponseEntity<List<Dish>> getTodayMeals(@PathVariable String canteenName) {
-            List<Dish> todayMeals = canteenService.getTodayMeals(canteenName);
-            return ResponseEntity.ok(todayMeals);
+    public ResponseEntity<List<Dish>> getTodayMeals(@PathVariable String canteenName) {
+        List<Dish> todayMeals = canteenService.getTodayMeals(canteenName);
+
+        if (todayMeals.isEmpty()) {
+            return ResponseEntity.noContent().build();
         }
+
+        return ResponseEntity.ok(todayMeals);
+    }
 }
